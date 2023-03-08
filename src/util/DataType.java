6def7584d63b29198abd0976859d835671044feb
수빈 <118293643+growupsb@users.noreply.github.com>
2023-03-08 16:10:02 +0900

    util
    util

--- src/util/DataType.java
+++ src/util/DataType.java
@@ -3,11 +3,14 @@ package util;
 import java.util.Scanner;
 
 public class DataType {
-	static Scanner sc=new Scanner(System.in);
+	public static Scanner sc=new Scanner(System.in);
 	public static int inputInt() {
 		return sc.nextInt();
 	}
 	public static String inputString() {
 		return sc.next();
+	}
+	public static String line() {
+		return sc.nextLine();
 	}
 }
